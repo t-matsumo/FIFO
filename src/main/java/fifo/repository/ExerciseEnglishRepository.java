d0437c4703ee17c2499ae24e9d2f04aa65b2e101
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2017-03-17 14:10:57 +0900

    Add ExerciseEnglish.java

---
+++ src/main/java/fifo/repository/ExerciseEnglishRepository.java
@@ -0,0 +1,8 @@
+package fifo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import fifo.entity.ExerciseEnglish;
+
+public interface ExerciseEnglishRepository extends CrudRepository<ExerciseEnglish, Long> {
+}
