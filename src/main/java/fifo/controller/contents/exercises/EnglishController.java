9c8e274ee9c449694c7e013b4a2415332836dc79
Tatsuki Matsumoto <tatsukimatsumo@gmail.com>
2017-03-17 13:22:01 +0900

    Add a result page

--- src/main/java/fifo/controller/contents/exercises/EnglishController.java
+++ src/main/java/fifo/controller/contents/exercises/EnglishController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/exercises/english")
 public class EnglishController {
   @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  public String getExercise(@PathVariable("id") String id, Model model) {
+  public String exercise(@PathVariable("id") String id, Model model) {
     // 問題をデータベースから取ってくる
     model.addAttribute("id", id);
     
@@ -22,12 +22,19 @@ public class EnglishController {
   }
 
   @RequestMapping(value = "/{id}", method = RequestMethod.POST)
-  public String getAnswer(@PathVariable("id") String id, Model model) {
+  public String answer(@PathVariable("id") String id, Model model) {
     // 答えをデータベースから取ってきて比較
     model.addAttribute("id", id);
     model.addAttribute("nextId", Long.parseLong(id) + 1L);
     
     // 答え合わせと次へボタンを返す
     return "contents/exercises/answer";
+  }
+
+  @RequestMapping(value = "/result", method = RequestMethod.GET)
+  public String result() {
+    // データベースから結果を取ってきて表示
+
+    return "contents/exercises/result";
   }
 }
